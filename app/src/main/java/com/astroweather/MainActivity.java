e7de0c978c35eb4413e4c4644d3717de6417d3f0
Anon <zalozonynaszybko@gmail.com>
2019-05-28 22:55:03 +0200

    added a welcome page with settings (WIP)

--- app/src/main/java/com/astroweather/MainActivity.java
+++ app/src/main/java/com/astroweather/MainActivity.java
@@ -1,52 +1,32 @@
 package com.astroweather;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button proceedbutton;
+    private TextView latitude;
+    private TextView longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        proceedbutton = findViewById(R.id.Proceed_button);
+        latitude = findViewById(R.id.Latitude_Value_editText);
+        longitude = findViewById(R.id.Longitude_Value_editText);
+
+        proceedbutton.setOnClickListener(v -> {
+            Intent nextScreen = new Intent(getApplicationContext(), ScreenSlideActivity.class);
+            nextScreen.putExtra("latitude", Float.parseFloat(latitude.getText().toString()));
+            nextScreen.putExtra("longitude", Float.parseFloat(longitude.getText().toString()));
+            startActivity(nextScreen);
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
