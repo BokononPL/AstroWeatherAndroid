720141c982bc701f1aeef517f7d623f407204f78
Anon <zalozonynaszybko@gmail.com>
2019-05-31 01:34:57 +0200

    added layout for information display for tablet portrait mode

--- app/src/main/java/com/astroweather/ScreenSlideActivity.java
+++ app/src/main/java/com/astroweather/ScreenSlideActivity.java
@@ -37,16 +37,34 @@ public class ScreenSlideActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slide_layout);
 
-        // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
-        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(pagerAdapter);
+
+        Fragment sunfragment = new ScreenSlideSunFragment();
+        Fragment moonfragment = new ScreenSlideMoonFragment();
+        Fragment statusfragment = new StatusDisplayFragment();
+
+        if(mPager == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fl1,sunfragment)
+                    .replace(R.id.fl2, moonfragment)
+                    .replace(R.id.fl3, statusfragment)
+                    .commit();
+        }
+        else {
+
+            // Instantiate a ViewPager and a PagerAdapter.
+
+            pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+            mPager.setAdapter(pagerAdapter);
+        }
 
         latitude = getIntent().getFloatExtra("latitude", 0);
         longitude = getIntent().getFloatExtra("longitude", 0);
         refreshrate = getIntent().getIntExtra("refreshrate", 1);
     }
 
+    /*
     @Override
     public void onBackPressed() {
         if (mPager.getCurrentItem() == 0) {
@@ -58,6 +76,7 @@ public class ScreenSlideActivity extends FragmentActivity {
             mPager.setCurrentItem(mPager.getCurrentItem() - 1);
         }
     }
+    */
 
     /**
      * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
---
+++ app/src/main/java/com/astroweather/StatusDisplayFragment.java
@@ -0,0 +1,67 @@
+package com.astroweather;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StatusDisplayFragment extends Fragment {
+
+    private float latitude = 0;
+    private float longitude = 0;
+    private ViewGroup rootView;
+    private TextView currentTime;
+    private TextView latview;
+    private TextView lonview;
+
+    private Timer timer;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = (ViewGroup) inflater.inflate(
+                R.layout.status_fragment_layout, container, false);
+
+        ScreenSlideActivity ssa = (ScreenSlideActivity) getActivity();
+        latitude = ssa.getLatitude();
+        longitude = ssa.getLongitude();
+
+        latview = rootView.findViewById(R.id.Status_Latitude_textView);
+        lonview = rootView.findViewById(R.id.Status_Longitude_textView);
+        currentTime = rootView.findViewById(R.id.Status_Time_textView);
+
+        TimerTask taskTime = new TimerTask() {
+            @Override
+            public void run() {
+                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                Calendar now = Calendar.getInstance();
+
+                getActivity().runOnUiThread(()->{
+                    currentTime.setText(dateFormat.format((now.getTime())));
+                });
+            }
+        };
+
+        latview.setText("Latitude:" + Float.toString(latitude));
+        lonview.setText("Longitude:" + Float.toString(longitude));
+
+        timer = new Timer(false);
+        timer.scheduleAtFixedRate(taskTime, 0, 1000);
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
+    }
+}
