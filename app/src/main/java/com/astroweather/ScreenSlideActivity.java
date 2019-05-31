ed953452c32bf21883f7cbb541b955c432b0ad4d
Anon <zalozonynaszybko@gmail.com>
2019-05-31 04:51:30 +0200

    finished layouts for both devices and both orientations

--- app/src/main/java/com/astroweather/ScreenSlideActivity.java
+++ app/src/main/java/com/astroweather/ScreenSlideActivity.java
@@ -21,23 +21,19 @@ public class ScreenSlideActivity extends FragmentActivity {
     public int getRefreshrate() {
         return refreshrate;
     }
-
     public float getLatitude() {
         return latitude;
     }
-
     public float getLongitude() {
         return longitude;
     }
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.slide_layout);
 
-        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager = findViewById(R.id.pager);
 
         Fragment sunfragment = new ScreenSlideSunFragment();
         Fragment moonfragment = new ScreenSlideMoonFragment();
@@ -52,9 +48,6 @@ public class ScreenSlideActivity extends FragmentActivity {
                     .commit();
         }
         else {
-
-            // Instantiate a ViewPager and a PagerAdapter.
-
             pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
             mPager.setAdapter(pagerAdapter);
         }
@@ -64,24 +57,6 @@ public class ScreenSlideActivity extends FragmentActivity {
         refreshrate = getIntent().getIntExtra("refreshrate", 1);
     }
 
-    /*
-    @Override
-    public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
-    }
-    */
-
-    /**
-     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-     * sequence.
-     */
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
         public ScreenSlidePagerAdapter(FragmentManager fm) {
             super(fm);
